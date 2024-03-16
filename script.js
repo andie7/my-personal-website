073fe4d3e91a1ac767e15eecc27d5178b8453af1
andie7 <131781648+andie7@users.noreply.github.com>
2024-03-16 19:45:41 +0100

    Added How I built it and create an Image Gallery with Horizontal scroll

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function handleSubmission(event) {
 let button = document.getElementById("submit-button");
 let form = document.getElementById("formElement");
 button.addEventListener("click", handleSubmission);
+
+// button
+
+function goRight() {
+  scrollContainer.scrollLeft += 900;
+}
+
+function goLeft() {
+  scrollContainer.scrollLeft -= 900;
+}
+
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+backBtn.addEventListener("click", goLeft.bind(scrollContainer));
+
+nextBtn.addEventListener("click", goRight.bind(scrollContainer));
