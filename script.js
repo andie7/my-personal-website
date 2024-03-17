42b16bdab5297f5481e43774d8d412c0d820864d
andie7 <131781648+andie7@users.noreply.github.com>
2024-03-17 08:45:23 +0100

    nav link and images changed

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function handleSubmission(event) {
   event.preventDefault();
 
   form.submit();
-  alert("Thank you for your message");
+  alert(
+    "Thank you for your message. The goal here was to use Java Script to alert you this message when the button is clicked. In case you want to contact me, please do it via  phone, email or Linkedin"
+  );
 }
 
 let button = document.getElementById("submit-button");
